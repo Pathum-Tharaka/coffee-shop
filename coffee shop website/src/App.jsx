5b19e97d0e5ec4ec05790861223e8e8cb663f09c
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-09-21 20:34:21 +0530

    image include

--- coffee shop website/src/App.jsx
+++ coffee shop website/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import './App.css'
+
 import heroImage from './assets/images/img4.jpg'
+import order from './assets/images/order.jpeg'
+import wait from './assets/images/wait.jpeg'
+import enjoy from './assets/images/enjoy.png'
+
 import Hero from './components/Hero'  // Make sure to import the Hero component
 import Navbar from './components/Navbar';
 import Process from './components/Process';
 import Footer from './components/Footer';
 
 const App = () => {
+  const cardDate = [{title: "Act",
+    description: "Choose the plan that works best for you.",
+    imageSrc:order},
+
+    {title: "Await",
+    description: "We are selecting the best coffee for you.",
+    imageSrc:wait},
+
+    {title: "Enjoy",
+      description: "Have coffee delivered right to your doorstep.",
+      imageSrc:enjoy},
+  
+  ]
   return (
     <>
     <Navbar> 
@@ -19,7 +37,7 @@ const App = () => {
         btnText="Shop Now"
         bgSrc={heroImage}
       />
-      <Process/>
+      <Process title={"Our Process" } cardDate={cardDate}/>
       <Footer />
     </>
   );
